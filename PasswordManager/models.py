0c8f18a94ac4600bff9d8cb3b642e75b5fb9d255
Justin <justinboisson@gmail.com>
2023-12-09 19:38:42 +0100

    Added the Site model

--- PasswordManager/models.py
+++ PasswordManager/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Site(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    nom = models.CharField(max_length=100)
+    url = models.URLField()
+    identifiant = models.CharField(max_length=100)
+    mot_de_passe = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nom
