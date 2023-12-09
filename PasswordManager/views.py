6284a6f46705ab089a1fbc6e24d9070d4b10c17a
Justin <justinboisson@gmail.com>
2023-12-09 20:11:57 +0100

    Added login page with Bootstrap 5 styling

--- PasswordManager/views.py
+++ PasswordManager/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+
+class Login(SuccessMessageMixin, LoginView):
+    template_name = 'registration/login.html'
+    success_message = "Vous êtes connecté avec succès !"
+    redirect_authenticated_user = True
