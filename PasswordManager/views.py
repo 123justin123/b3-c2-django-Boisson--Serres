c159901bd0b40bfced39e8b906384934029aa833
Justin <justinboisson@gmail.com>
2023-12-10 14:47:27 +0100

    Added delete_site, edit_site, and Dashboard views to enhance site management functionality.

--- PasswordManager/views.py
+++ PasswordManager/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 from django.urls import reverse_lazy
+from .models import Site
+from .forms import SiteForm
 
 
 class Login(SuccessMessageMixin, LoginView):
@@ -40,4 +42,30 @@ def Signup(request):
 
 @login_required
 def Dashboard(request):
-    return render(request, 'dashboard.html')
+    site_form = SiteForm(request.POST or None)
+    if request.method == 'POST' and site_form.is_valid():
+        new_site = site_form.save(commit=False)
+        new_site.user = request.user
+        new_site.save()
+        return redirect('dashboard')
+
+    sites = Site.objects.filter(user=request.user)
+    return render(request, 'dashboard.html', {'site_form': site_form, 'sites': sites})
+
+
+@login_required
+def edit_site(request, site_id):
+    site = get_object_or_404(Site, id=site_id, user=request.user)
+    site_form = SiteForm(request.POST or None, instance=site)
+    if request.method == 'POST' and site_form.is_valid():
+        site_form.save()
+        return redirect('dashboard')
+
+    return render(request, 'dashboard.html', {'site_form': site_form, 'edit_site': site})
+
+
+@login_required
+def delete_site(request, site_id):
+    site = get_object_or_404(Site, id=site_id, user=request.user)
+    site.delete()
+    return redirect('dashboard')
--- password_manager/urls.py
+++ password_manager/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from PasswordManager.views import Login, Signup, Dashboard, Logout
+from PasswordManager.views import Login, Signup, Dashboard, Logout, edit_site, delete_site
 
 urlpatterns = [
+    path('', Login.as_view(), name='root'),
     path('admin/', admin.site.urls),
     path('accounts/login/', Login.as_view(), name='login'),
     path('accounts/signup/', Signup, name='signup'),
     path('accounts/logout/', Logout.as_view(), name='logout'),
     path('dashboard/', Dashboard, name='dashboard'),
+    path('edit-site/<int:site_id>/', edit_site, name='edit_site'),
+    path('delete-site/<int:site_id>/', delete_site, name='delete_site'),
 ]
